dd59199716e7d2b93e992919f91d2f188f895144
Bosco Barber Esbert <boscobarberesbert@gmail.com>
2023-01-10 20:50:32 +0100

    Heatmap finished

--- Assets/Scripts Game Analytics/Heatmap.cs
+++ Assets/Scripts Game Analytics/Heatmap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
-public class Grid
+public class Heatmap
 {
     public int HEAT_MAP_MAX_VALUE = 100;
     public const int HEAT_MAP_MIN_VALUE = 0;
@@ -14,21 +14,19 @@ public class Grid
     private Vector3 originPosition;
     private int[,] gridArray;
     private TextMesh[,] debugTextArray;
-    private Vector3[,] positionArray; // The actual data!
+    private Vector3[] positionArray = new Vector3[0]; // The actual data!
+    private Gradient gradient;
 
-    public Grid(int width, int height, float cellSize, Vector3 originPosition, GameObject cubeGO)
+    public Heatmap(int width, int height, float cellSize, Vector3 originPosition, Gradient gradient, GameObject cubeGO)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
+        this.gradient = gradient;
 
         gridArray = new int[width, height];
         debugTextArray = new TextMesh[width, height];
-
-        Gradient gradient;
-        GradientColorKey[] colorKey;
-        GradientAlphaKey[] alphaKey;
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
@@ -48,34 +46,11 @@ public class Grid
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.black, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.black, 100f);
 
+        // Get the heatmap max value in order to normalize the values afterwards
+        GetHeatmapMaxValue(positionArray);
+
         // Generate the cubes in their position and with their proper color
         GenerateCubes(cubeGO, positionArray);
-
-        // Setting up the gradient
-        gradient = new Gradient();
-
-        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
-        colorKey = new GradientColorKey[3];
-        colorKey[0].color = Color.red;
-        colorKey[0].time = 0.0f;
-        colorKey[1].color = Color.green;
-        colorKey[1].time = 0.5f;
-        colorKey[2].color = Color.blue;
-        colorKey[2].time = 1.0f;
-
-        // Populate the alpha keys at relative time 0 and 1 (0 and 100%)
-        alphaKey = new GradientAlphaKey[3];
-        alphaKey[0].alpha = 0.5f;
-        alphaKey[0].time = 0.0f;
-        alphaKey[1].alpha = 0.5f;
-        alphaKey[1].time = 0.5f;
-        alphaKey[2].alpha = 0.5f;
-        alphaKey[2].time = 1.0f;
-
-        gradient.SetKeys(colorKey, alphaKey);
-
-        // What's the color at the relative time 0.25 (25%) ?
-        // Debug.Log(gradient.Evaluate(0.25f));
     }
 
     private Vector3 GetWorldPosition(int x, int y)
@@ -134,20 +109,37 @@ public class Grid
         SetValue(x, y, GetValue(x, y) + 1);
     }
 
-    // Generate the cubes in their position and with their proper color
-    void GenerateCubes(GameObject cubeGO, Vector3[,] positionArray)
+    // Get the heatmap max value in order to normalize the values afterwards
+    private void GetHeatmapMaxValue(Vector3[] positionArray)
     {
-        // THIS IS NOT RIGHT!
-        for (int x = 0; x < gridArray.GetLength(0); x++)
+        foreach (Vector3 v in positionArray)
         {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
-            {
-                GameObject cube = GameObject.Instantiate(cubeGO);
-                cube.transform.localScale *= cellSize;
-                cube.transform.position = GetWorldPosition(x, y) + new Vector3(cellSize, 0, cellSize) * .5f;
+            int x, y;
+            GetXY(v, out x, out y);
 
-                cube.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.5f);
-            }
+            int value = GetValue(x, y);
+            if (value > HEAT_MAP_MAX_VALUE)
+                HEAT_MAP_MAX_VALUE = value;
+        }
+    }
+
+    // Generate the cubes in their position and with their proper color
+    private void GenerateCubes(GameObject cubeGO, Vector3[] positionArray)
+    {
+        foreach (Vector3 v in positionArray)
+        {
+            int x, y;
+            GetXY(v, out x, out y);
+
+            GameObject cube = GameObject.Instantiate(cubeGO);
+            cube.transform.localScale *= cellSize;
+            cube.transform.position = GetWorldPosition(x, y) + new Vector3(cellSize, 0, cellSize) * .5f;
+
+            // Setting up the gradient color
+            float time = GetValue(x, y) / HEAT_MAP_MAX_VALUE;
+            Color gradientColor = gradient.Evaluate(time);
+
+            cube.GetComponent<Renderer>().material.color = gradientColor;
         }
     }
 }
--- Assets/Scripts Game Analytics/Testing.cs
+++ Assets/Scripts Game Analytics/Testing.cs
@@ -8,11 +8,26 @@ public class Testing : MonoBehaviour
     public GameObject playerGO;
     public GameObject cubeGO;
 
-    private Grid grid;
+    private Heatmap grid;
+    public Gradient gradient = new Gradient
+    {
+        alphaKeys = new[]
+        {
+            new GradientAlphaKey(0.5f, 0f),
+            new GradientAlphaKey(0.5f, 1f)
+        },
+
+        colorKeys = new[]
+        {
+            new GradientColorKey(Color.red, 0f),
+            new GradientColorKey(Color.green, 0.5f),
+            new GradientColorKey(Color.blue, 1f)
+        }
+    };
 
     private void Start()
     {
-        grid = new Grid(44, 32, 2.5f, new Vector3(-35,0,-40), cubeGO);
+        grid = new Heatmap(44, 32, 2.5f, new Vector3(-35,0,-40), gradient, cubeGO);
     }
 
     private void Update()
